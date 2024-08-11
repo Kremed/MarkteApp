49eb08ca607300945d523f2d497d5b82ad34247c
Kremed <moatasemkremed@yahoo.com>
2024-08-11 20:08:38 +0200

    Adding Admin Endpoints

--- MarkteApp.Backend/Controllers/CurrencyController.cs
+++ MarkteApp.Backend/Controllers/CurrencyController.cs
@@ -12,23 +12,123 @@ namespace MarkteApp.Backend.Controllers
     public class CurrencyController(CurrenciyDbContext db) : ControllerBase
     {
 
-        [HttpPost("CreateCurrency")]
-        public async Task<IActionResult> CreateCurrency([FromBody] Currency NewCurrency)
+        //نقطة النهاية الخاصة بأستجلاب جميع العملات من جدول العملات
+        // This Endpoint Is only for Admins but we can use it for all apps
+        [HttpGet("admin/getCurrencies")]
+        public async Task<IActionResult> getCurrencies()
         {
+                                          
+            var currenciesList = await db.Currencies
+                                .Where(c => c.IsActive == true)
+                                .ToListAsync();
 
-            var SameData = await db.Currencies
-                       .Where(x => x.Name == NewCurrency.Name || x.Code == NewCurrency.Code)
-                       .ToListAsync();
+            return Ok(currenciesList);
+        }
 
-            if (SameData.Count() != 0)
+        //نقطة النهاية الخاصة بأضافة عملة جديدة لجدول العملات
+        // This Endpoint Is only for Admins
+
+        [HttpPost("admin/createCurrency")]
+        public async Task<IActionResult> createCurrency([FromBody] Currency newCurrency)
+        {
+            
+            var CurrencyFromDataBase = await db.Currencies
+                                        .Where(x => x.Name == newCurrency.Name || x.Code == newCurrency.Code)
+                                        .FirstOrDefaultAsync();
+
+            if (CurrencyFromDataBase is not null)
             {
-                return BadRequest("العنصر الذي تريد ادراجه موجود مسبقــا");
+                return BadRequest("العنصر الذي تريد ادراجه موجود مسبقــا, الرجاء التأكد من الاسم والكود.");
             }
 
-            await db.Currencies.AddAsync(NewCurrency);
-            await db.SaveChangesAsync();
-            return Ok("تم أضافة العملية الجديدة بنجاح");
 
+            //01) - make validation on currency =>
+            var result = Currency.Validate(newCurrency);
+            if (result != "OK")
+            {
+                return BadRequest(result);
+            }
+
+            await db.Currencies.AddAsync(newCurrency);
+
+            await db.SaveChangesAsync();
+
+            return Ok("تم أضافة العملية الجديدة بنجاح");
         }
+
+
+        [HttpDelete("admin/deleteCurrency")]
+        public async Task<IActionResult> deleteCurrency(int currencyID)
+        {
+            var CurrencyFromDataBase = await db.Currencies
+                                               .Where(x => x.Id == currencyID)
+                                               .FirstOrDefaultAsync();
+            if (CurrencyFromDataBase is null)
+            {
+                return BadRequest("العنصر الذي تريد حدفه غير موجود في قواعد البيانات !!");
+            }
+
+            db.Currencies.Remove(CurrencyFromDataBase);
+
+            await db.SaveChangesAsync();
+
+            return Ok("تم حذف العملة من جدول العملات بنجاح");
+        }
+
+        [HttpPost("admin/updateCurrency")]
+        public async Task<IActionResult> updateCurrency([FromBody] Currency currency)
+        {
+            var CurrencyFromDataBase = await db.Currencies
+                                               .Where(x => x.Id == currency.Id)
+                                               .FirstOrDefaultAsync();
+
+            //01) - make validation on currency =>
+            if (CurrencyFromDataBase is null)
+            {
+                return NotFound("العملة الذي تريد التعديل عليها غير موجودة, الرجاء ادراج العملة للتمكن من التعديل");
+            }
+            else if (string.IsNullOrEmpty(currency.Name))
+            {
+                return BadRequest("لايمكن اضافة عملة بدون أسم, الرجاء كتابة اسم العملية");
+            }
+            else if (string.IsNullOrEmpty(currency.Description))
+            {
+                return BadRequest("الرجاء تزود النظام بوصف العملة, أضافة وصف يساعد في تقديم تجربة مستخدم جيدة");
+            }
+            //Code نقوم بأجراء تحققات علي حقل 
+            //من الضروري ان يكون الكود غير فارغا
+            //من الضروري ان يكون الكود يحتوي علي 6 خانات تدل علي العملة والعملة المقابلة
+            //USD X | LYD x | (USDLYD) OK | (TUNLYD) OK
+            else if (string.IsNullOrEmpty(currency.Code) ||
+                                         currency.Code.Length != 6)
+            {
+
+                //API REQUEST Curreny ISO Codes Provider 
+                //Get All Currencis startWhit currency.Code
+
+                return BadRequest($"كود العملة غير مطابق, الرجاء تحديد كود عملة بالحروف اللاتينية ويجب ان يتكون من 6 خانات مثلا: (TULYD)");
+            }
+            else if (string.IsNullOrEmpty(currency.ImageUrl) ||
+                     !currency.ImageUrl.StartsWith("https://") ||
+                     (!currency.ImageUrl.EndsWith(".png") && !currency.ImageUrl.EndsWith(".jpeg") && !currency.ImageUrl.EndsWith(".jpg")))
+            {
+                return BadRequest("صورة العملة يجب ان تكون رابط صالح, الرجاء التأكد من صورة العملة.");
+            }
+            
+            //02) - Update the existing currency in the database
+            CurrencyFromDataBase.Name = currency.Name;
+            CurrencyFromDataBase.Description = currency.Description;
+            CurrencyFromDataBase.Code = currency.Code;
+            CurrencyFromDataBase.IsActive = currency.IsActive;
+            CurrencyFromDataBase.ImageUrl = currency.ImageUrl;
+
+            //CurrencyFromDataBase = currency;
+            
+            await db.SaveChangesAsync();
+
+            return Ok($"تمت عملية تعديل البيانات الخاصة بعملة: {CurrencyFromDataBase.Name} بنجاح.");
+        }
+
     }
 }
+
--- MarkteApp.Backend/Models/Currency.cs
+++ MarkteApp.Backend/Models/Currency.cs
@@ -17,8 +17,30 @@ public partial class Currency
 
     public bool IsActive { get; set; }
 
-    public void Tost()
-    {
 
+    public static string Validate(Currency currency)
+    {
+        if (string.IsNullOrEmpty(currency.Name))
+        {
+            return "لايمكن اضافة عملة بدون أسم, الرجاء كتابة اسم العملية";
+        }
+        else if (string.IsNullOrEmpty(currency.Description))
+        {
+            return "الرجاء تزود النظام بوصف العملة, أضافة وصف يساعد في تقديم تجربة مستخدم جيدة";
+        }
+        else if (string.IsNullOrEmpty(currency.Code) ||
+                                     currency.Code.Length != 6)
+        {
+            return $"كود العملة غير مطابق, الرجاء تحديد كود عملة بالحروف اللاتينية ويجب ان يتكون من 6 خانات مثلا: (TULYD)";
+        }
+        else if (string.IsNullOrEmpty(currency.ImageUrl) ||
+                 !currency.ImageUrl.StartsWith("https://") ||
+                 (!currency.ImageUrl.EndsWith(".png") && !currency.ImageUrl.EndsWith(".jpeg") && !currency.ImageUrl.EndsWith(".jpg")))
+        {
+            return "صورة العملة يجب ان تكون رابط صالح, الرجاء التأكد من صورة العملة.";
+        }
+
+        return "OK";
     }
+
 }
